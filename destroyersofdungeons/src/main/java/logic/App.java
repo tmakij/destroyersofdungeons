ec231026ed2455a984fa637e602fd7e962d63d35
tmaki <finyou2@gmail.com>
2013-12-16 18:35:46 +0200

    Commit from home, gui git, testing javadocs!

--- destroyersofdungeons/src/main/java/logic/App.java
+++
@@ -1,8 +0,0 @@
-package logic;
-
-public final class App {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
---
+++ destroyersofdungeons/src/main/java/main/App.java
@@ -0,0 +1,14 @@
+package main;
+
+import logic.NewClass;
+
+final class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Testing from home!");
+        System.out.println("\\o/");
+        NewClass nc = new NewClass();
+        nc.print(23);
+    }
+}
